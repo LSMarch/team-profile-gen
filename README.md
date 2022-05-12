72f0fc41a7759b9ca3b3e8d9cd5225d833832d6e
Libby Marchetti <lsmarchetti01@gmail.com>
2022-05-11 20:12:59 -0500

    fixed write and append files, code cleanup

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# team-profile-gen
+# Team Profile Generator ![Badge for license](https://img.shields.io/badge/license-mit-brightgreen) 
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [How-To](#how-to)
+- [Contribution](#contribution)
+- [Testing Instructions](#testing-instructions)
+- [Contact](#contact)
+- [License](#license)
+  
+### [Description](#table-of-contents)
+
+A command line application run with Inquirer creates an HTML page with the company employees. Just answer the prompts and instantly get a formatted, style HTML page.
+
+### [Installation](#table-of-contents)
+
+To run the application you will need `Node.js`, as well as the `Inquirer`, and `Jest` packages.
+
+### [How-To](#table-of-contents)
+
+To use Team Profile Generator just type `node index.js` and answer the prompts.
+
+### [Contribution](#table-of-contents)
+
+Pull requests are welcome! For any major changes please open an issue.
+
+### [Testing Instructions](#table-of-contents)
+
+Install Jest and run the tests 
+
+### [Contact](#table-of-contents)
+
+Questions?
+Comments?
+Concerns?
+    
+- [LSMarch](https://github.com/LSMarch)
+- lsmarchetti01@gmail.com
+
+   
+    
+### [License](#table-of-contents)
+    
+MIT License
+
+Copyright (c) 2022 Libby Marchetti
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 
--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const fs = require('fs')
 const inquirer = require('inquirer')
-const generateHTML = require('./src/generateHTML')
 const Manager = require('./lib/manager')
 const Engineer = require('./lib/engineer')
 const Intern = require('./lib/intern')
 
 const teamRoster = []
 
-const inputManager =  () => {
-    return inquirer.prompt([
+const init = () => {
+    inputTeam();
+    beginHTML();
+};
+
+function inputTeam() {
+    inquirer.prompt([
         {
             type: 'input',
             name: 'name',
-            message: "Enter team manager's name",
+            message: "Enter team member's name",
             validate: function (answer) {
                 if (answer = !/[a-z]/.test(answer)) {
                     console.log('Please enter a valid name')
@@ -20,6 +24,16 @@ const inputManager =  () => {
                 }
                 return true
             }
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: "Enter team member's role",
+            choices: [
+                "Manager",
+                "Engineer",
+                "Intern"
+            ]
         },
         {
             type: 'number',
@@ -44,153 +58,160 @@ const inputManager =  () => {
                 }
                 return true
             }
-        },
-        {
-            type: 'number',
-            name: 'office',
-            message: 'What is the office number?',
-            validate: function (answer) {
-                if (answer= !/[1-9]/.test(answer)) {
-                    return false
-                }
-                return true
-            }
-        },
+        }
     ])
-    .then(addManager => {
-        const { name, id, email, office } = addManager
-        const manager = new Manager(name, id, email, office)
-        teamRoster.push(manager)
-        console.log(manager)
-    })    
-}
-
-const inputEmployee = () => {
-    console.log(`
-    ====================
-    Adding Team Memebers
-    ====================
-    `)
-    return inquirer.prompt([
-        {
-            type: 'list',
-            name: 'position',
-            message: 'Please enter employee position',
-            choices: ['Engineer', 'Intern']
-        },
-        {
-            
+    .then(function({name, role, id, email}) {
+        let roleInfo = ''
+        if(role ==='Manager') {
+            roleInfo = 'office number'
+        } else if (role === 'Engineer') {
+            roleInfo = 'GitHub username'
+        } else {
+            roleInfo = 'school name'
+        }
+        inquirer.prompt([
+            {
                 type: 'input',
-                name: 'name',
-                message: "Enter team member's name",
-                validate: function (answer) {
-                    if(answer = !/[a-z]/.test(answer)) {
-                        console.log('Please enter a valid name')
-                        return false
-                    }
-                    return true;
-                }
+                name: 'roleInfo',
+                message: `Enter team member's ${roleInfo}`
             },
-            {
-                type: 'number',
-                name: 'id',
-                message: 'Enter valid employee ID number',
-                validate: function (answer) {
-                    if(answer = !/[1-9]/.test(answer)) {
-                        console.log('Please enter a valid employee ID')
-                        return false
-                    }
-                    return true
-                }
-            },
-            {
-                type: 'input',
-                name: 'email',
-                message: 'Enter valid email address',
-                validate: answer => {
-                    if(answer = !/\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/.test(answer)){
-                        console.log('Please enter valid email')
-                        return false
-                    }
-                    return true
-                }
-            },             
-            {
-                type: 'input',
-                name: 'github',
-                message: 'Please enter GitHub username',
-                when: (input) => input.position === 'Engineer',
-                validate: function (answer) {
-                    if(answer = !/[a-z]/.test(answer)) {
-                        console.log('Please enter a valid name')
-                        return false
-                    }
-                    return true;
-                }
-            }, 
-            {
-                type: 'input',
-                name: 'school',
-                message: 'Please enter school name',
-                when: (input) => input.position === 'Intern',   
-                validate: function (answer) {
-                    if(answer = !/[a-z]/.test(answer)) {
-                        console.log('Please enter a valid name')
-                        return false
-                    }
-                    return true;
-                }             
-            },
-            {
+            {            
                 type: 'confirm',
                 name: 'addMore',
                 message: 'Would you like to add another team memeber?',
-                default: false
-            },
-        
-    ])
-    .then(teamData => {
-        let {name, id, email, position, github, school, addMore} = teamData
-        let teamMember
-
-        if (position === 'Engineer') {
-            teamMember = new Engineer (name, id, email, github)
-            //console.log(teamMember)
-        } else if (position === 'Intern') {
-            teamMember = new Intern (name, id, email, school)
-            //console.log(teamMember)
-        }
-        teamRoster.push(teamMember)
-
-        if (addMore) {
-            return inputEmployee(teamRoster)
-        } else {
-            return teamRoster
-        }
+                default: false                
+            }
+        ])
+        .then(function({roleInfo, addMore}) {
+            let teamMember;
+            if (role === 'Manager') {
+                teamMember = new Manager (name, id, email, roleInfo);
+            } else if (role === 'Engineer') {
+                teamMember = new Engineer (name, id, email, roleInfo);
+            } else {
+                teamMember = new Intern (name, id, email, roleInfo)
+            }
+            teamRoster.push(teamMember);
+            addTeam(teamMember)
+            .then(function() {
+                if (addMore) {
+                    inputTeam();
+                } else {
+                    endHTML();
+                }
+            });
+        });                
     })
-}
+};
 
-const writeFile = data => {
-    fs.writeFile('./dist/index.html', data, err => {
+const beginHTML = () => {
+    const html = `
+    <!doctype html>
+    <html lang="en">
+        <head>    
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <script src="https://kit.fontawesome.com/308e3ed2db.js" crossorigin="anonymous"></script>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+            <title>Hello, Team!</title>
+        </head>
+        <body>
+            <header class="jumbotron jumbotron-fluid text-center bg-danger text-light">
+                <div class="container">
+                    <h1 class="display-4">Meet the Team</h1>          
+                </div>
+            </header>
+            <main class="container d-flex justify-content-center flex-wrap">
+                <div class="row">
+                    <!--Team-->
+    `;
+    fs.writeFile('./dist/index.html', html, function(err){
         if (err) {
             console.log(err)
-            return
-        } else {
-            console.log('Team page created!')
         }
+    })
+    //console.log('begin')
+}
+
+const addTeam = (member) => {
+    return new Promise(function(resolve, reject) {
+        const name = member.getName();
+        const role = member.getRole();
+        const id = member.getId();
+        const email = member.getEmail();
+        let data = '';
+        if(role==='Manager') {
+            const office = member.getOfficeNumber();
+            data = `
+            <div class="card m-4" style="width: 18rem;">
+            <div class="card-header bg-primary text-white">
+                <h2>${name}</h2>
+                <h4>Manager</h4>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID:${id}</li>
+                <li class="list-group-item"><a href="mailto:${email}">${email}</a></li>
+                <li class="list-group-item">Office Number:${office}</li>
+            </ul>
+        </div>
+            `
+        } else if (role==='Engineer') {
+            const github = member.getGithub();
+            data = `
+            <div class="card m-4" style="width: 18rem;">
+            <div class="card-header bg-primary text-white">
+                <h2>${name}</h2>
+                <h4>Engineer</h4>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID:${id}</li>
+                <li class="list-group-item"><a href="mailto:${email}">${email}</a></li>
+                <li class="list-group-item">GitHub:${github}</li>
+            </ul>
+        </div>
+            `
+        } else {
+            const school = member.getSchool();
+            data = `
+            <div class="card m-4" style="width: 18rem;">
+            <div class="card-header bg-primary text-white">
+                <h2>${name}</h2>
+                <h4>Intern</h4>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID:${id}</li>
+                <li class="list-group-item"><a href="mailto:${email}">${email}</a></li>
+                <li class="list-group-item">School:${school}</li>
+            </ul>
+        </div>
+            `
+        }
+        console.log('adding team');
+        fs.appendFile('./dist/index.html', data, function(err) {
+            if (err) {
+                return reject(err);
+            };
+            return resolve();
+        })
     })
 }
 
-inputManager()
-    .then(inputEmployee)
-    .then(teamRoster =>{
-        return generateHTML(teamRoster)
+const endHTML = () => {
+    const html = `
+    </div>
+            </main>    
+            <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>    
+        </body>
+    </html>
+    `;
+    fs.appendFile('./dist/index.html', html, function (err) {
+        if (err) {
+            console.log(err);
+        };        
     })
-    .then(teamPageHTML => {
-        return writeFile(teamPageHTML)
-    })
-    .catch(err => {
-        console.log(err)
-    })
- 
+    console.log('finished');
+}
 
+
+init();
